0eb62ba096be2db9e70df332607d276bd103d689
sunnyin <sunnyin127@gmail.com>
2016-09-01 15:39:32 +0800

    Update run.py

--- VOA/Scripts/run.py
+++ VOA/Scripts/run.py
@@ -2,16 +2,25 @@
 
 from Client import VoaClient
 from urlparse import urlparse
+import os,datetime
 
-
+timeNow = datetime.datetime.now().strftime('%Y%m%d%H')
 client = VoaClient()
 url = "http://www.51voa.com/VOA_Special_English/"
 urlScheme = urlparse(url).scheme 
 urlNet = urlparse(url).netloc  
-ip = client.ipList()
+ipList = client.ipList()
 userAgent = client.userAgent()
-bsObj = client.conn(url,ip,userAgent)
-# print(bsObj)
-urls = client.urls(bsObj)
-    url = urlScheme + "://" + urlNet + url
-    print(url)
+bsObj_proxy = client.conn(url,ipList,userAgent)
+
+urls = client.urls(bsObj_proxy)
+
+for _ in urls:
+    url_voa = urlScheme + "://" + urlNet + url
+    bsObj_voa = client.conn(url_voa,ipList,userAgent)
+    contents = client.getContents(bsObj_voa)
+    file = open("/home/sunyin/Data/"+timeNow,'a+')
+    for _ in contents:
+        file.write(str(_))
+
+file.close()
