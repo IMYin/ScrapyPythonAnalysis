90d4baeea3d339066aabe614bafb0a5736891b5f
sunnyin <sunnyin127@gmail.com>
2016-09-01 23:39:52 +0800

    run

--- VOA/Scripts/run.py
+++ VOA/Scripts/run.py
@@ -15,15 +15,15 @@ userAgent = client.userAgent()
 
 num = [x for x in range(16)[1:]]
 for _ in num:
-    url = "http://www.51voa.com/Technology_Report_"+_+".html"
+    url = "http://www.51voa.com/Technology_Report_"+str(_)+".html"
     bsObj_proxy = client.conn(url,ipList,userAgent)
     urls = client.urls(bsObj_proxy)
     for q in urls:
         url_voa = urlScheme + "://" + urlNet + q
         bsObj_voa = client.conn(url_voa,ipList,userAgent)
         contents = client.getContents(bsObj_voa)
-        file = open("/home/sunyin/Data/"+timeNow,'a+')
+        file = open("/home/sunnyin/Project/Python/ProjectOfSelf/VOA/Data"+timeNow,'a+')
         for word in contents:
-            file.write(str(word))
+            file.write(word.encode('utf-8'))
         file.close()
-        time.sleep(1)
+        time.sleep(2)
